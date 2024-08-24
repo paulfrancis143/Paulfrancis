cdda0f47f29acf42faf9c7f764007f505fe57a5c
paulfrancis143 <99167224+paulfrancis143@users.noreply.github.com>
2024-08-24 00:40:38 -0400

    Update Container.cs
    implementation of bind with if else and use both variables

--- Container/Container.cs
+++ Container/Container.cs
@@ -1,10 +1,57 @@
 using System;
+using System.Collections.Generic;
 
 namespace DeveloperSample.Container
 {
     public class Container
     {
-        public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-        public T Get<T>() => throw new NotImplementedException();
+        private readonly Dictionary<Type, Type> _bindings = new();
+        public void Bind(Type interfaceType, Type implementationType)
+        {
+            if (interfaceType == null) throw new ArgumentNullException(nameof(interfaceType));
+            if (implementationType == null) throw new ArgumentNullException(nameof(implementationType));
+            if (!interfaceType.IsInterface) throw new ArgumentException("interfaceType must be an interface.", nameof(interfaceType));
+            if (!interfaceType.IsAssignableFrom(implementationType)) throw new ArgumentException("implementationType does not implement interfaceType.", nameof(implementationType));
+
+            _bindings[interfaceType] = implementationType;
+        }
+        
+        public T Get<T>()
+        {
+            var type = typeof(T);
+            if (!_bindings.TryGetValue(type, out var implementationType))
+            {
+                throw new InvalidOperationException($"Type {type} not registered.");
+            }
+
+            return (T)CreateInstance(implementationType);
+        }
+
+        // Create an instance of the type using reflection
+        private object CreateInstance(Type type)
+        {
+            var constructor = type.GetConstructors()[0];
+            var parameters = constructor.GetParameters();
+
+            var parameterInstances = new object[parameters.Length];
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                var parameterType = parameters[i].ParameterType;
+                var parameterInstance = GetService(parameterType);
+                parameterInstances[i] = parameterInstance;
+            }
+
+            return Activator.CreateInstance(type, parameterInstances);
+        }
+        
+        private object GetService(Type serviceType)
+        {
+            if (!_bindings.TryGetValue(serviceType, out var implementationType))
+            {
+                throw new InvalidOperationException($"Service {serviceType} not registered.");
+            }
+
+            return CreateInstance(implementationType);
+        }
     }
 }
