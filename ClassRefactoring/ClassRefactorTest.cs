8087075be0468162bc3ceef339e3d2ea27a2f68d
paulfrancis143 <99167224+paulfrancis143@users.noreply.github.com>
2024-08-24 00:11:17 -0400

    Update ClassRefactorTest.cs
    using inline and theory instead of fact

--- ClassRefactoring/ClassRefactorTest.cs
+++ ClassRefactoring/ClassRefactorTest.cs
@@ -4,38 +4,23 @@ namespace DeveloperSample.ClassRefactoring
 {
     public class ClassRefactorTest
     {
-        [Fact]
-        public void AfricanSwallowHasCorrectSpeed()
+        [Theory]
+        [InlineData(SwallowType.African, SwallowLoad.None, 22)]
+        [InlineData(SwallowType.African, SwallowLoad.Coconut, 18)]
+        [InlineData(SwallowType.European, SwallowLoad.None, 20)]
+        [InlineData(SwallowType.European, SwallowLoad.Coconut, 16)]
+        public void SwallowHasCorrectSpeed(SwallowType type, SwallowLoad load, double expectedSpeed)
         {
+            // Arrange
             var swallowFactory = new SwallowFactory();
-            var swallow = swallowFactory.GetSwallow(SwallowType.African);
-            Assert.Equal(22, swallow.GetAirspeedVelocity());
-        }
-
-        [Fact]
-        public void LadenAfricanSwallowHasCorrectSpeed()
-        {
-            var swallowFactory = new SwallowFactory();
-            var swallow = swallowFactory.GetSwallow(SwallowType.African);
-            swallow.ApplyLoad(SwallowLoad.Coconut);
-            Assert.Equal(18, swallow.GetAirspeedVelocity());
-        }
-
-        [Fact]
-        public void EuropeanSwallowHasCorrectSpeed()
-        {
-            var swallowFactory = new SwallowFactory();
-            var swallow = swallowFactory.GetSwallow(SwallowType.European);
-            Assert.Equal(20, swallow.GetAirspeedVelocity());
-        }
-
-        [Fact]
-        public void LadenEuropeanSwallowHasCorrectSpeed()
-        {
-            var swallowFactory = new SwallowFactory();
-            var swallow = swallowFactory.GetSwallow(SwallowType.European);
-            swallow.ApplyLoad(SwallowLoad.Coconut);
-            Assert.Equal(16, swallow.GetAirspeedVelocity());
+            var swallow = swallowFactory.GetSwallow(type);
+            swallow.ApplyLoad(load);
+            
+            // Act
+            var actualSpeed = swallow.GetAirspeedVelocity();
+            
+            // Assert
+            Assert.Equal(expectedSpeed, actualSpeed);
         }
     }
 }
